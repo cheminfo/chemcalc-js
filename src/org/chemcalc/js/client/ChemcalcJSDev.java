789125469974df233a71a0b8dd1072c70d73bbab
Michaël Zasso <mic.besace@gmail.com>
2015-01-12 08:30:48 +0100

    add dev environment

---
+++ src/org/chemcalc/js/client/ChemcalcJSDev.java
@@ -0,0 +1,11 @@
+package org.chemcalc.js.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class ChemcalcJSDev implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+	}
+
+}
